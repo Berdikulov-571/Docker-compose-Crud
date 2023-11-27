5dda0ff689d4f6cb231cf9d1afb8b06672bae652
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:19:49 +0500

    IAppartmentRepository

---
+++ AppartmentSystem.Service/Interfaces/IAppartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using AppartmentSystem.Domain.Entities;
+
+namespace AppartmentSystem.Service.Interfaces
+{
+    public interface IAppartmentRepository
+    {
+        ValueTask CreateAsync(AppartmentModel model);
+        ValueTask DeleteAsync(int appartmentId);
+        ValueTask<IEnumerable<AppartmentModel>> GetAllAsync();
+    }
+}

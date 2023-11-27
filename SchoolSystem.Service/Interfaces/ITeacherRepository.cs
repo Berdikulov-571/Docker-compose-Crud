11c3ccc43205c436b37eacebbe0a70ee14afdb08
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:21:24 +0500

    ITeacherRepository

---
+++ SchoolSystem.Service/Interfaces/ITeacherRepository.cs
@@ -0,0 +1,11 @@
+﻿using SchoolSystem.Domain.Entities;
+
+namespace SchoolSystem.Service.Interfaces
+{
+    public interface ITeacherRepository
+    {
+        ValueTask CreateAsync(Teacher teacher);
+        ValueTask DeleteAsync(int teacherId);
+        ValueTask<IEnumerable<Teacher>> GetAllAsync();
+    }
+}

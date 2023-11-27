764734cee91a8931edad2684a9d1c212cd206ba4
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:19:36 +0500

    AppartmentModel

---
+++ AppartmentSystem.Domain/Entities/AppartmentModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace AppartmentSystem.Domain.Entities
+{
+    public class AppartmentModel
+    {
+        [Key]
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public int Number { get; set; } = default!;
+        public int Floor { get; set; } = default!;
+        public int Room { get; set; } = default!;
+        public string Location { get; set; } = default!;
+    }
+}

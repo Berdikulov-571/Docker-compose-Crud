91b97da6975b069433ae048906ad8e5b80d9e5a4
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:20:45 +0500

    ApplicationDbContext

---
+++ SchoolSystem.DataAccess/DataContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Domain.Entities;
+
+namespace AppartmentSystem.DataAccess.DataContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Teacher> Teachers { get; set; }
+        public virtual DbSet<Class> Classes { get; set; }
+    }
+}

59c7e7239405bd98580eb95b5d3c646816d9093c
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:20:27 +0500

    CarModel

---
+++ CarSystem.Domain/Entities/CarModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CarSystem.Domain.Entities
+{
+    public class CarModel
+    {
+        [Key]
+        [JsonIgnore]
+        public int CartId { get; set; }
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}

ae42b9c97b595038a3cdd1cbcd34e0d6def37cbb
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:20:52 +0500

    Class

---
+++ SchoolSystem.Domain/Entities/Class.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SchoolSystem.Domain.Entities
+{
+    public class Class
+    {
+        [Key]
+        [JsonIgnore]
+        public int ClassId { get; set; }
+        public string ClassName { get; set; } = default!;
+        [ForeignKey(nameof(ClassTeacher))]
+        public int TeacherId { get; set; } = default!;
+        public Teacher ClassTeacher { get; set; } = default!;
+    }
+}

74ea0f4caedf8eaeae51ea9d9993902ee4b7cecc
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:19:25 +0500

    ApplicationDbContext

---
+++ AppartmentSystem.DataAccess/DataContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AppartmentSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppartmentSystem.DataAccess.DataContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<AppartmentModel> Appartments { get; set; }
+    }
+}

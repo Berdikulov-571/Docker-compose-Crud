b51d627d9fb51b87d90a04f07989047bc2557a17
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:20:34 +0500

    ClassController

---
+++ SchoolSystem.Api/Controllers/ClassController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolSystem.Domain.Entities;
+using SchoolSystem.Service.Interfaces;
+
+namespace SchoolSystem.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ClassController : ControllerBase
+    {
+        private readonly IClassRepository _repo;
+
+        public ClassController(IClassRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> PostAsync(Class @class)
+        {
+            await _repo.CreateAsync(@class);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int classId)
+        {
+            await _repo.DeleteAsync(classId);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            IEnumerable<Class> classes = await _repo.GetAllAsync();
+
+            return Ok(classes);
+        }
+    }
+}

8f8a9bd6fb179ff9bdd34d35f2d523def72527e8
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:20:11 +0500

    Program.cs

---
+++ AppartmentSystem.Api/Program.cs
@@ -0,0 +1,40 @@
+using AppartmentSystem.DataAccess.DataContexts;
+using AppartmentSystem.Service.Interfaces;
+using AppartmentSystem.Service.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppartmentSystem.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IAppartmentRepository, AppartmentService>();
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

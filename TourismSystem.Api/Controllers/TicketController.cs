c6f2150f61072b51b5fc73288d83662c1904d7c5
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:52:26 +0500

    TicketController

---
+++ TourismSystem.Api/Controllers/TicketController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TourismSystem.Domain.Entities;
+using TourismSystem.Service.Interfaces;
+
+namespace TourismSystem.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITickerRepository _repo;
+
+        public TicketController(ITickerRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> PostAsync(Ticket ticket)
+        {
+            await _repo.CreateAsync(ticket);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int ticketId)
+        {
+            await _repo.DeleteAsync(ticketId);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            IEnumerable<Ticket> classes = await _repo.GetAllAsync();
+
+            return Ok(classes);
+        }
+
+    }
+}

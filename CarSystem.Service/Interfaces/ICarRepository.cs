0ed768841f55e956a8617f43480beb39921e4df9
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:24:59 +0500

    ICarRepository

---
+++ CarSystem.Service/Interfaces/ICarRepository.cs
@@ -0,0 +1,11 @@
+﻿using CarSystem.Domain.Entities;
+
+namespace CarSystem.Service.Interfaces
+{
+    public interface ICarRepository
+    {
+        ValueTask CreateAsync(CarModel model);
+        ValueTask DeleteAsync(int CarId);
+        ValueTask<IEnumerable<CarModel>> GetAllAsync();
+    }
+}

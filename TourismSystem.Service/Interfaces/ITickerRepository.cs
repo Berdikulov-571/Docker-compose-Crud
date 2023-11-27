15c77d155b799d5bff91f2e59d4273618417115a
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:52:56 +0500

    ITicketRepository

---
+++ TourismSystem.Service/Interfaces/ITickerRepository.cs
@@ -0,0 +1,11 @@
+﻿using TourismSystem.Domain.Entities;
+
+namespace TourismSystem.Service.Interfaces
+{
+    public interface ITickerRepository
+    {
+        ValueTask CreateAsync(Ticket ticker);
+        ValueTask DeleteAsync(int ticketId);
+        ValueTask<IEnumerable<Ticket>> GetAllAsync();
+    }
+}

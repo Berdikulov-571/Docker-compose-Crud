8af5e3f90a23d96aa301ed86bd8886a7aa4fbb32
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:52:35 +0500

    Program.cs

--- TourismSystem.Api/Program.cs
+++ TourismSystem.Api/Program.cs
@@ -1,3 +1,8 @@
+using AppartmentSystem.DataAccess.DataContexts;
+using Microsoft.EntityFrameworkCore;
+using TourismSystem.Service.Interfaces;
+using TourismSystem.Service.Services;
+
 namespace TourismSystem.Api
 {
     public class Program
@@ -6,16 +11,16 @@ namespace TourismSystem.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<ITickerRepository, TickerService>();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();

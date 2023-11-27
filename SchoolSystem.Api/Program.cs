c05c64911f0cfbab2c09fb452c4b818c025ddc97
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:22:03 +0500

    Program.cs

---
+++ SchoolSystem.Api/Program.cs
@@ -0,0 +1,41 @@
+using AppartmentSystem.DataAccess.DataContexts;
+using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Service.Interfaces;
+using SchoolSystem.Service.Services;
+
+namespace SchoolSystem.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<ITeacherRepository, TeacherService>();
+            builder.Services.AddScoped<IClassRepository, ClassService>();
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

123fa81a41fe8f85e45141c9bbe6a8f3f4802863
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:21:02 +0500

    IClassRepository

---
+++ SchoolSystem.Service/Interfaces/IClassRepository.cs
@@ -0,0 +1,11 @@
+﻿using SchoolSystem.Domain.Entities;
+
+namespace SchoolSystem.Service.Interfaces
+{
+    public interface IClassRepository
+    {
+        ValueTask CreateAsync(Class model);
+        ValueTask DeleteAsync(int classId);
+        ValueTask<IEnumerable<Class>> GetAllAsync();
+    }
+}

72789cd70ba99aecb16ad9bd4b237e5e4e145cc7
berdikulov-571 <mygithub571@gmail.com>
2023-11-27 16:22:20 +0500

    Ticket

---
+++ TourismSystem.Domain/Entities/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace TourismSystem.Domain.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        [JsonIgnore]
+        public int TicketId { get; set; }
+        public string Where { get; set; }
+        public string WhereTo { get; set; }
+        public decimal Price { get; set; }
+    }
+}
